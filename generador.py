30bb39beae2f9abd59eec9acb32cd9deb88590ef
Carlos Jose Rosales Santos <carlos.jose.rosales@estudiantat.upc.edu>
2025-01-13 18:45:08 +0100

    Generador modificado para que coincida con el formateo de los .pddl

--- generador.py
+++ generador.py
@@ -1,14 +1,14 @@
 import random
-# import os
 import sys
 
 def gen_base_case():
     # Genera un caso base
-    name = "base_" + str(random.randint(1, 100))
+    name = "juego-de-prueba-basico-" + str(random.randint(1, 100))
     print(name, file= sys.stdout)
+
     with open(name + ".pddl", "w") as f:
         # Inicializamos el domnio
-        print("(define (problem "+name+")", file=f)
+        print("(define (problem " + name + ")", file=f)
         print("\t(:domain planner)", file=f)
         numContenidos = random.randint(5, 10)
         
@@ -36,19 +36,19 @@ def gen_base_case():
             while assig.get(j, False):
                 j = str(random.randint(0, numContenidos-1))
             assig[j] = True
-            print("\t\t(quiere_ver C"+j+")", file=f) 
+            print("\t\t(quiere-ver C"+j+")", file=f) 
         print("\t)", file=f)
         print(file=f)
-        print("\t(:goal (and (forall (?c - contenido) (imply (quiere_ver ?c) (ha_visto ?c)))))", file=f)
+        print("\t(:goal (and (forall (?c - contenido) (imply (quiere-ver ?c) (ha-visto ?c)))))", file=f)
         print(")",file=f)
 
 def gen_ext1_case():
     # Genera un caso base
-    name = "Ext1_" + str(random.randint(1, 100))
+    name = "juego-de-prueba-ext1-" + str(random.randint(1, 100))
     print(name, file= sys.stdout)
     with open(name + ".pddl", "w") as f:
         # Inicializamos el domnio
-        print("(define (problem "+name+")", file=f)
+        print("(define (problem " + name + ")", file=f)
         print("\t(:domain plannerExt1)", file=f)
         numContenidos = random.randint(5, 15)
         
@@ -75,7 +75,7 @@ def gen_ext1_case():
         
         for i in range(numContenidos):
             for j in range(i+1, numContenidos):
-                print("\t\t(diaAnterior D"+str(i) +" D"+str(j)+")", file=f)
+                print("\t\t(dia-anterior D"+str(i) +" D"+str(j)+")", file=f)
             
         print("\t)", file=f)
         print(file=f)
@@ -87,18 +87,18 @@ def gen_ext1_case():
             while assig.get(j, False):
                 j = str(random.randint(0, numContenidos-1))
             assig[j] = True
-            print("\t\t(ha_visto C"+j+")", file=f)
+            print("\t\t(ha-visto C"+j+")", file=f)
         print("\t\t)", file=f)
         print("\t)", file=f)
         print(")",file=f)
 
 def gen_ext2_case():
     # Genera un caso base
-    name = "Ext2_" + str(random.randint(1, 100))
+    name = "juego-de-prueba-ext2-" + str(random.randint(1, 100))
     print(name, file= sys.stdout)
     with open(name + ".pddl", "w") as f:
         # Inicializamos el domnio
-        print("(define (problem "+name+")", file=f)
+        print("(define (problem " + name + ")", file=f)
         print("\t(:domain plannerExt2)", file=f)
         numContenidos = random.randint(5, 15)
         
@@ -135,12 +135,12 @@ def gen_ext2_case():
 
         print(file=f)
         for i in range(numContenidos-1):
-            print("\t\t(diaSiguiente D"+str(i+1) +" D"+str(i)+")", file=f)
+            print("\t\t(dia-siguiente D"+str(i+1) +" D"+str(i)+")", file=f)
 
         print(file=f)
         for i in range(numContenidos):
             for j in range(i+1, numContenidos):
-                print("\t\t(diaAnterior D"+str(i) +" D"+str(j)+")", file=f)
+                print("\t\t(dia-anterior D"+str(i) +" D"+str(j)+")", file=f)
             
         print("\t)", file=f)
         print(file=f)
@@ -152,18 +152,18 @@ def gen_ext2_case():
             while assig.get(j, False):
                 j = str(random.randint(0, numContenidos-1))
             assig[j] = True
-            print("\t\t(ha_visto C"+j+")", file=f)
+            print("\t\t(ha-visto C"+j+")", file=f)
         print("\t\t)", file=f)
         print("\t)", file=f)
         print(")",file=f)
 
 def gen_ext3_case():
     # Genera un caso base
-    name = "Ext3_" + str(random.randint(1, 100))
+    name = "juego-de-prueba-ext3-" + str(random.randint(1, 100))
     print(name, file= sys.stdout)
     with open(name + ".pddl", "w") as f:
         # Inicializamos el domnio
-        print("(define (problem "+name+")", file=f)
+        print("(define (problem "+ name +")", file=f)
         print("\t(:domain plannerExt3)", file=f)
         numContenidos = random.randint(5, 15)
         
@@ -200,17 +200,17 @@ def gen_ext3_case():
 
         print(file=f)
         for i in range(numContenidos-1):
-            print("\t\t(diaSiguiente D"+str(i+1) +" D"+str(i)+")", file=f)
+            print("\t\t(dia-siguiente D"+str(i+1) +" D"+str(i)+")", file=f)
 
         print(file=f)
         for i in range(numContenidos):
             for j in range(i+1, numContenidos):
-                print("\t\t(diaAnterior D"+str(i) +" D"+str(j)+")", file=f)
+                print("\t\t(dia-anterior D"+str(i) +" D"+str(j)+")", file=f)
 
         print(file=f)
-        # numAsignados
+        # num-asignados
         for i in range(numContenidos):
-            print("\t\t(= (numAsignados D" + str(i)+") 0)", file= f)
+            print("\t\t(= (num-asignados D" + str(i)+") 0)", file= f)
 
 
         print("\t)", file=f)
@@ -223,18 +223,18 @@ def gen_ext3_case():
             while assig.get(j, False):
                 j = str(random.randint(0, numContenidos-1))
             assig[j] = True
-            print("\t\t(ha_visto C"+j+")", file=f)
+            print("\t\t(ha-visto C"+j+")", file=f)
         print("\t\t)", file=f)
         print("\t)", file=f)
         print(")",file=f)
         
 def gen_ext4_case():
     # Genera un caso base
-    name = "Ext4_" + str(random.randint(1, 100))
+    name = "juego-de-prueba-ext4-" + str(random.randint(1, 100))
     print(name, file= sys.stdout)
     with open(name + ".pddl", "w") as f:
         # Inicializamos el domnio
-        print("(define (problem "+name+")", file=f)
+        print("(define (problem " + name + ")", file=f)
         print("\t(:domain plannerExt4)", file=f)
         numContenidos = random.randint(5, 15)
         
@@ -271,20 +271,20 @@ def gen_ext4_case():
 
         print(file=f)
         for i in range(numContenidos-1):
-            print("\t\t(diaSiguiente D"+str(i+1) +" D"+str(i)+")", file=f)
+            print("\t\t(dia-siguiente D"+str(i+1) +" D"+str(i)+")", file=f)
 
         print(file=f)
         for i in range(numContenidos):
             for j in range(i+1, numContenidos):
-                print("\t\t(diaAnterior D"+str(i) +" D"+str(j)+")", file=f)
+                print("\t\t(dia-anterior D"+str(i) +" D"+str(j)+")", file=f)
 
         print(file=f)
         for i in range(numContenidos):
-            print("\t\t(= (duracionContenido C" + str(i)+") "+str(random.randint(10, 100))+")", file= f)
+            print("\t\t(= (duracion-contenido C" + str(i)+") "+str(random.randint(10, 100))+")", file= f)
         
         print(file=f)
         for i in range(numContenidos):
-            print("\t\t(= (duracionDiaria D" + str(i)+") 0)", file= f)
+            print("\t\t(= (duracion-diaria D" + str(i)+") 0)", file= f)
 
         print("\t)", file=f)
         print(file=f)
@@ -296,7 +296,7 @@ def gen_ext4_case():
             while assig.get(j, False):
                 j = str(random.randint(0, numContenidos-1))
             assig[j] = True
-            print("\t\t(ha_visto C"+j+")", file=f)
+            print("\t\t(ha-visto C"+j+")", file=f)
         print("\t\t)", file=f)
         print("\t)", file=f)
         print(")",file=f)
